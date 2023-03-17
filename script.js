2d069864016918b9d5be550b17f8417fa6e1c0ac
JassiSingh08 <singhjassi693@gmail.com>
2023-03-17 22:07:59 +0530

    latest commit

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 var character = document.getElementById('character');
 var block = document.getElementById('block'); 
+var scorebox = document.getElementById('score');
+let score = 0;
+var start = document.getElementById('start');
+var checkdead = true;
 
 
 window.addEventListener('keydown', function(event) {
@@ -7,7 +11,27 @@ window.addEventListener('keydown', function(event) {
     console.log('Arrow up key pressed!');
     character.classList.add("animate");
     this.setTimeout(()=>{
-        character.classList.remove('animate');
+      character.classList.remove('animate');
     },500)
   }
 });
+
+
+var checkdead = setInterval(function(){
+  var charactertop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+  var blockleft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+  
+  if(blockleft<20 && blockleft>0 && charactertop>=180){
+    block.classList.remove('animateblock');
+    alert("Game Over. Score is " +score);
+    score = 0;
+    scorebox.innerHTML = "Score : "+score;
+  }
+  },10);
+  
+  
+start.addEventListener('click',function start(){
+  block.classList.add('animateblock');
+});
+
+
