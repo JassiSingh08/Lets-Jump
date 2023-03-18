fa535ae1588ea739e29fe32522b154a05a50ae0c
JassiSingh08 <singhjassi693@gmail.com>
2023-03-19 02:15:13 +0530

    latest commits

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 var character = document.getElementById('character');
 var block = document.getElementById('block'); 
 var scorebox = document.getElementById('score');
+// var highscorebox = document.getElementById('Highscore');
+// let hiscoreval = 0;
 let score = 0;
 var start = document.getElementById('start');
-var checkdead = true;
+
+
 
 
 window.addEventListener('keydown', function(event) {
   if (event.code === 'ArrowUp' || event.code === 'Space') {
-    console.log('Arrow up key pressed!');
+    console.log('Arrow up or Space key pressed!');
     character.classList.add("animate");
     this.setTimeout(()=>{
       character.classList.remove('animate');
@@ -17,21 +20,30 @@ window.addEventListener('keydown', function(event) {
 });
 
 
+// To Start Button
+
+window.addEventListener('click',function(){
+  block.classList.add('animateblock');
+  setInterval(() => {
+    score++;
+    scorebox.innerHTML = "Score : "+score;
+  }, 300);
+});
+
+
+
+// check for Game Over 
+
 var checkdead = setInterval(function(){
   var charactertop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
   var blockleft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
   
-  if(blockleft<20 && blockleft>0 && charactertop>=180){
+  if(blockleft<50 && blockleft>0 && charactertop>=180){
     block.classList.remove('animateblock');
-    alert("Game Over. Score is " +score);
+    alert("Game Over. Score is " +score+ ".");
     score = 0;
     scorebox.innerHTML = "Score : "+score;
+    location.reload()
   }
-  },10);
   
-  
-start.addEventListener('click',function start(){
-  block.classList.add('animateblock');
-});
-
-
+},10);
